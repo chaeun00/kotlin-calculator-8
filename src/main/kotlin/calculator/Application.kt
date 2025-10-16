0b1a7d66f7af3bc79eb9b3aef067dc2559c081dc
Chaeun Kim <kimchaeun111@gmail.com>
2025-10-16 16:11:07 +0900

    feat: 빈 문자열 또는 null 입력 시 0 출력 기능 구현

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,7 +1,19 @@
 package calculator
 
+fun calculate(text: String?): Int {
+    if (text.isNullOrBlank()) {
+        return 0
+    }
+
+    // 테스트 코드
+    return 1
+}
+
+
 fun main() {
     // TODO: 프로그램 구현
     println("덧셈할 문자열을 입력해 주세요.")
     val str = readLine()
+
+    println("결과 : " + calculate(str))
 }
